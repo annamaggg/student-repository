c16e87f660c51dccbb4455dcdee64f91ec9250c8
annamaggg <annamagnusson99@gmail.com>
2023-01-10 18:21:08 +0000

    input names

--- directory.rb
+++ directory.rb
@@ -1,22 +1,36 @@
-student_count = 11
-#first we print the list of students
-students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
-]
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-    puts student
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+
+    students = []
+    name = gets.chomp
+
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "now we have #{students.count} students"
+        name = gets.chomp
+    end
+
+    students
 end
+
+def print_header
+    puts "The students of Villains Academy"
+    puts "-------------"
+end
+
+def print(students)
+    students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+end
+
+def print_footer(names)
 # finally, we print the total number of students
-puts "Overall, we have #{students.count} great students" 
+    puts "Overall, we have #{names.count} great students" 
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
